0a62f3a8723a7d49fa8f24983b70339da2908fc9
Tao <62868403+Pixel-Tao@users.noreply.github.com>
2024-09-30 21:41:26 +0900

    [Update] 키 잘 못입력했을때 프로그램 종료하는 부분 그냥 반복해서 다시 입력하도록 수정함.

--- Team7TextRPG/Managers/InputManager.cs
+++ Team7TextRPG/Managers/InputManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Team7TextRPG.Utils;
 using static Team7TextRPG.Utils.Defines;
 
 namespace Team7TextRPG.Managers
@@ -33,22 +34,37 @@ namespace Team7TextRPG.Managers
         // 입력받은 문자열을 대문자로 변환하고 _keyword에 해당하는지 확인해서 반환하는 함수
         public string GetInputKeyword(string message = "키워드를 입력하세요.")
         {
-            string input = GetInputString(message).ToUpper();
-            if (_keywords.Contains(input) == false)
-                throw new Exception("입력 가능한 문자가 아닙니다.");
+            while (true)
+            {
+                string input = GetInputString(message).ToUpper();
+                if (_keywords.Contains(input) == false)
+                {
+                    TextHelper.DtContent("잘못 된 입력입니다.");
+                    continue;
+                }
 
-            return input;
+                return input;
+            }
         }
         // 입력받은 문자열을 int로 변환해서 반환하는 함수
         public int GetInputInt(string message = "숫자를 입력하세요.", int minValue = 0, int maxValue = 9)
         {
-            if (int.TryParse(GetInputString(message), out int value) == false)
-                throw new Exception("입력 가능한 숫자가 아닙니다.");
+            while (true)
+            {
+                if (int.TryParse(GetInputString(message), out int value) == false)
+                {
+                    TextHelper.DtContent("잘못 된 입력입니다.");
+                    continue;
+                }
 
-            if (value < minValue || value > maxValue)
-                throw new Exception("입력 가능한 숫자 범위가 아닙니다.");
+                if (value < minValue || value > maxValue)
+                {
+                    TextHelper.DtContent("잘못 된 입력입니다.");
+                    continue;
+                }
 
-            return value;
+                return value;
+            }
         }
         // 입력받은 문자열을 enum으로 변환해서 반환하는 함수
         public T GetInputType<T>(string message = "숫자를 입력하세요.", int defaultValue = 0) where T : Enum
@@ -67,11 +83,17 @@ namespace Team7TextRPG.Managers
         public T ParseInputType<T>(string input) where T : Enum
         {
             if (int.TryParse(input, out int value) == false)
-                throw new Exception("입력 가능한 숫자가 아닙니다.");
+            {
+                TextHelper.DtContent("잘못 된 입력입니다.");
+                return (T)Enum.ToObject(typeof(T), 0);
+            }
 
             T[] values = (T[])Enum.GetValues(typeof(T));
             if (values.Length <= value || value <= 0)
-                throw new Exception("입력 가능한 숫자 범위가 아닙니다.");
+            {
+                TextHelper.DtContent("잘못 된 입력입니다.");
+                return (T)Enum.ToObject(typeof(T), 0);
+            }
 
             return (T)Enum.ToObject(typeof(T), value);
         }
